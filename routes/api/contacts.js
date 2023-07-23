74a66e1941a25393be7cb8c73310bfe2b9f2f666
Serhii-Buha <serhii.buga@gmail.com>
2023-07-23 12:56:37 +0300

    added routers f authenticate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -7,19 +7,20 @@ const {
   updateById,
   updateFavorite,
 } = require("../../controllers/contacts");
+const authenticate = require("../../middleware");
 
 const router = express.Router();
 
-router.get("/", getAll);
+router.get("/", authenticate, getAll);
 
-router.get("/:contactId", getId);
+router.get("/:contactId", authenticate, getId);
 
-router.post("/", add);
+router.post("/", authenticate, add);
 
-router.delete("/:contactId", deleteById);
+router.delete("/:contactId", authenticate, deleteById);
 
-router.put("/:contactId", updateById);
+router.put("/:contactId", authenticate, updateById);
 
-router.patch("/:contactId/favorite", updateFavorite);
+router.patch("/:contactId/favorite", authenticate, updateFavorite);
 
 module.exports = router;
