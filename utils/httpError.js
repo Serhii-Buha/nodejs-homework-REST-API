d44decc07934342e1d1632f60847547acbb4365f
Serhii-Buha <serhii.buga@gmail.com>
2023-07-23 12:55:16 +0300

    added errorMessageList, ref httpError

--- utils/httpError.js
+++ utils/httpError.js
@@ -1,4 +1,15 @@
-const httpError = (status, message) => {
+const errorMessageList = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+  200: "OK",
+  201: "Created",
+  204: "No Content",
+};
+
+const httpError = (status, message = errorMessageList[status]) => {
   const error = new Error(message);
   error.status = status;
   return error;
@@ -7,4 +18,3 @@ const httpError = (status, message) => {
 module.exports = {
   httpError,
 };
-    
